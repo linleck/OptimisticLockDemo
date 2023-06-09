dbde83f3690b52a63248baedcc2f31cc629b742a
linleck <linleckkyalsin26@gmail.com>
2023-06-09 13:37:46 +0630

    ネストするTransactionalを修正する

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 
 @Service
+@Transactional
 public class StudentService {
     private final StudentRepository studentRepository;
 
@@ -15,16 +16,14 @@ public class StudentService {
         this.studentRepository = productRepository;
     }
 
-    @Transactional
     public Student createStudent(Student student){
         return studentRepository.save(student);
     }
-    @Transactional
     public Student updateStudentAge(Student updateStudent) {
         Student student = studentRepository.findById(updateStudent.getId()).orElseThrow(() -> new IllegalArgumentException("Student not found"));
         if (updateStudent.getVersion() == student.getVersion()){
             student.updateAge(updateStudent.getAge());
-            return createStudent(student);
+            return studentRepository.save(student);
         } else {
             throw new OptimisticLockException("Expected version = "+updateStudent.getVersion()+"but got "+student.getVersion()+". Concurrent modification detected");
         }
